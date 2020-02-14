9bf2e84cb844ede7bcab4597b017c6003e2e1dfa
unknown <jacob.e.england@gmail.com>
2020-02-14 10:24:00 -0600

    Finished tabs.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// Store topics element
+topics = document.querySelector('.topics');
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(topic => topics.append(Tabulator(topic)));
+    })
+
+// Tab component function
+const Tabulator = (topic) => {
+    // Create element
+    let tab = document.createElement('div');
+    // Assign class
+    tab.classList.add('tab');
+    // Add content
+    tab.textContent = topic;
+    // Return element
+    return tab;
+}
